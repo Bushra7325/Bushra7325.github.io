5a886b2bba4120afbf4b86aab592ddf3c98f5b7a
Bushra Akhtar <71028246+Bushra7325@users.noreply.github.com>
2020-10-07 13:01:29 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
 $name=$_POST['name'];
-$visitor_email=$_POST['email'];
+$email=$_POST['email'];
 $message=$_POST['message'];
 
 
@@ -8,11 +8,11 @@ $message=$_POST['message'];
 $email_from='bushraakhtar271@gmail.com';
 $email_subject="New Form Submission";
 $email_body="User Name: $name.\n".
-             "User Email: $visitor_email.\n".
+             "User Email: $email.\n".
               "User Message: $message.\n";
 $to = "bushraakhtar271@gmail.com";
 $headers = "From: $email_from\r\n";
-$headers = "Reply-To: $visitor_email\r\n";
+$headers = "Reply-To: $email\r\n";
 mail($to,$email_subject,$email_body,$headers);
 header("Location: index.html");
 ?>
